ed44f8c0e45c6c0e218d783b1846d86716944e91
linguoze <linguoze@mtty.com>
2017-03-29 14:15:07 +0800

    fix server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express=require("express");
 var server = express()
 var fs = require("fs")
 
-server.use(express.static("resources"));
-server.get("/",function(req,res){
+server.use("/resources",express.static("resources"));
+server.get("(/|/homepage.html)",function(req,res){
     res.sendFile(__dirname+"/homepage.html");
 })
 
-server.listen(80,function(){
+server.listen(8000,function(){
     console.log("blog zxcchen.me is now running!!");
 });
