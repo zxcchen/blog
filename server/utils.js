bf0c20e1c3dc7da1a7d9da4e8c2097b118793353
zxcchen <2812349771@qq.com>
2017-07-03 01:31:21 +0800

    modified utils.js

--- server/utils.js
+++ server/utils.js
@@ -217,8 +217,8 @@ if (require.main === module) {
         console.log("testcase:", arr, ",key:", key, ",found:", bSearch(arr, key));
     }
     db.globalInit().then(function () {
-        let dirname = "D:\\Application\\cygwin\\home\\azywrmor\\Workspace\\qiqiblog\\resources\\html\\webFontEnd\\programming-tool\\jQuery_note.html"; 
-        //let dirname = path.join(__dirname, "../resources/html/webFontEnd/IFE_baidu.html");
+        //let dirname = "D:\\Application\\cygwin\\home\\azywrmor\\Workspace\\qiqiblog\\resources\\html\\webFontEnd\\programming-tool\\jQuery_note.html"; 
+        let dirname = path.join(__dirname, "../resources/html");
         loadBlogPostToDB(dirname,true).then(function () {
             db.globalRelease();
         }).catch(function () {
